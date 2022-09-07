1169732c1223d4237fa341bd00176e03cec5172c
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-09-07 17:36:58 +0300

    Note repository

---
+++ app/src/main/java/com/asimodabas/my_colornotes/feature_note/domain/repository/NotesRepository.kt
@@ -0,0 +1,16 @@
+package com.asimodabas.my_colornotes.feature_note.domain.repository
+
+import com.asimodabas.my_colornotes.feature_note.domain.model.Notes
+import kotlinx.coroutines.flow.Flow
+
+interface NotesRepository {
+
+
+    fun getNote(): Flow<List<Notes>>
+
+    suspend fun getNotesById(id: Int): Notes?
+
+    suspend fun insertNotes(note: Notes)
+
+    suspend fun deleteNotes(note: Notes)
+}
